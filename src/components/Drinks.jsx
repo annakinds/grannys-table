56aaf81e6ead53db2ade77eabdb62b05fde5f55a
Anna Kinds <anna.kinds@student.howest.be>
2025-03-27 13:52:05 +0100

    choose a drink

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -4,14 +4,25 @@ const Drinks = ({drinks}) => {
         <div className="drinks">
             <h2 className="drinks__title">Drinks</h2>
             <div className="recommended">
-                <h3>Recommended drinks</h3>
-                <ul>
+                <h3 className="recommended__title">Recommended drinks</h3>
+                <ul className="recommended__drinks">
                     {drinks.map(drink =>(
-                        <li key={drink.id}>
+                        <li key={drink.id} className="recommended__drink">
                             {drink.name}
                         </li>
                     ))}
                 </ul>
+            </div>
+            <div class="dropdown">
+                <label htmlFor="" className="dropdown__title">Choose a drink:</label>
+                <select class="dropdown__content">
+                    {/* <option value="">chosen drink</option> */}
+                    {drinks.map(drink => (
+                        <option key={drink.id} className="drink">
+                            {drink.name}
+                        </option>
+                    ))}
+                </select>
             </div>
         </div>
     );
